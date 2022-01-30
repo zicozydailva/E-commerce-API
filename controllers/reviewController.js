e0d2e90eafbb9e4dfa9f6cfc8b9499a339312ede
Ogbu Ezekiel Ike <zico@Macbooks-MacBook-Pro.local>
2022-01-30 23:12:39 +0100

    add createReviews functionality

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,21 +1,46 @@
-const createReview = (req, res) => {
-  console.log("create review");
+const Review = require("../models/Reviews")
+const Product = require("../models/Product")
+const { checkPermissions } = require("../utils");
+
+
+const createReview = async (req, res) => {
+  const {product: productId} = req.body;
+
+  const isValidProduct = await Product.findOne({_id: productId})
+
+  if(!isValidProduct) {
+    res.status(404).json("No product found")
+  }
+
+  const alreadySumitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId
+  })
+
+  if(alreadySumitted) {
+    res.status(401).json("Bad Request..Already submiited review for this product")
+  }
+
+  req.body.user = req.user.userId
+  const review = await Review.create(req.body)
+
+  res.status(200).json(review)
 };
 
 const getAllReview = (req, res) => {
-  console.log("All review");
+  res.send("All review");
 };
 
 const getSingleReview = (req, res) => {
-  console.log("single review");
+  res.send("single review");
 };
 
 const updateReview = (req, res) => {
-  console.log("update review");
+  res.send("update review");
 };
 
 const deleteReview = (req, res) => {
-  console.log("create review");
+  res.send("delete review");
 };
 
 module.exports = {
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,17 +2,17 @@ const {
   createReview,
   updateReview,
   deleteReview,
+  getAllReview,
+  getSingleReview,
 } = require("../controllers/reviewController");
-
-const { getAllUsers, getSingleUser } = require("../controllers/userController");
 
 const { authenticateUser } = require("../middleware/authentication");
 
 const router = require("express").Router();
 
-router.get("/", getAllUsers);
+router.get("/", getAllReview);
 router.post("/", authenticateUser, createReview);
-router.get("/:id", getSingleUser);
+router.get("/:id", getSingleReview);
 router.patch("/:id", authenticateUser,  updateReview);
 router.delete("/:id", authenticateUser, deleteReview);
 

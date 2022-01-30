453b082538e00da50d65729d08ac69d4a2cee800
Ogbu Ezekiel Ike <zico@Macbooks-MacBook-Pro.local>
2022-01-30 22:19:25 +0100

    upload image functionality and reviews modelling

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/Product");
+const path = require("path")
 
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId;
@@ -37,12 +38,27 @@ const deleteProduct = async (req, res) => {
   !product &&
     res.status(404).json(` product with ID: ${req.params.id} not found`);
 
-    await product.remove()
-    res.status(200).json("Successfully deleted product");
+  await product.remove();
+  res.status(200).json("Successfully deleted product");
 };
 
 const uploadImage = async (req, res) => {
-  res.send("upload image");
+  if (!req.files) {
+    res.status(400).json("No file uploaded");
+  }
+  const productImage = req.files.image;
+  if (!productImage.mimetype.startsWith("image")) {
+    res.status(400).json("Plese upload image");
+  }
+
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    res.status(400).json("Please upload image smaller than 1MB");
+  }
+
+  const imagePath = path.join(__dirname, "../public/uploads/" + `${productImage.name}`)
+  await productImage.mv(imagePath)
+  res.status(200).json({image: `/uploads/${productImage.name}`})
 };
 
 module.exports = {
